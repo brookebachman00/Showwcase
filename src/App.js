cc32273ef71039d6f828ba55ad75177773c6d0ac
brookebachman00 <bbachman1994@gmail.com>
2020-09-17 12:24:00 -0600

    got the values to properly filter

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import AutoCompleteText from './components/AutoCompleteText.js'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <AutoCompleteText></AutoCompleteText>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/AutoCompleteText.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+export default class AutoCompleteText extends React.Component {
+	constructor(props) {
+		super(props);
+		this.schools = ['San Francisco State', 'SFSU', 'yellow'];
+		this.state = {
+            suggestions: [],
+            text: '',
+		};
+	}
+
+	//i know i want to filter the list by the user's input
+	//i want to take that selection and put it
+	onTextChange = (event) => {
+		const value = event.target.value;
+		let suggestions = [];
+		if (value.length > 0) {
+			const regex = new RegExp(`^${value}`, 'i');
+			suggestions = this.schools.sort().filter(v => regex.test(v));
+		}
+		this.setState(() => ({ suggestions, text: value }));
+	};
+
+	renderSuggestions() {
+		const { suggestions } = this.state;
+		if (suggestions.length === 0) {
+			return null;
+		}
+		return (
+			<ul>
+				{suggestions.map((school) => 
+					<li >{school}</li>
+				)}
+			</ul>
+		);
+	}
+
+
+    //onClick={(school) => this.suggestionSelection(school)}
+	suggestionSelection(value) {
+		this.setState({
+			text: value,
+			suggestions: [],
+		});
+	}
+	render() {
+		const { text } = this.state;
+		return (
+			<div>
+				<input value={text} onChange={this.onTextChange} />
+                {this.renderSuggestions()}
+			</div>
+		);
+	}
+}
