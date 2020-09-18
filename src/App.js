ef3af08d2be0ab2f6714386f691d327c043deb9e
brookebachman00 <bbachman1994@gmail.com>
2020-09-17 18:05:55 -0600

    I got the http request for the api, to give me the information I am looking for

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
-import AutoCompleteText from './components/AutoCompleteText.js'
+import AutoCompleteText from './components/AutoCompleteText.js';
 import './App.css';
+import Home from './components/Home.js';
 
-function App() {
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      schools: []
+    }
+  }
 
-  // componentDidMount(){
-    
-  // }
-  return (
-    <div className="App">
-    <AutoCompleteText></AutoCompleteText>
-    </div>
-  );
+	componentDidMount() {
+    const results = []
+    fetch('http://universities.hipolabs.com/search?name=state'
 
-
+    ).then(resp => resp.json())
+    .then(data => data.forEach(element => {
+      results.push(element.name)
+      
+    }))
+   this.setState({
+     schools: results
+   })
+   
+  }
+	render() {
+		return (
+			<div className="App">
+				<Home schools={this.state.schools}></Home>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/AutoCompleteText.js
+++ src/components/AutoCompleteText.js
@@ -1,37 +1,36 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
-import Welcome from './Welcome.js'
 
 export default class AutoCompleteText extends React.Component {
 	constructor(props) {
 		super(props);
 		this.schools = ['San Francisco State', 'SFSU', 'yellow'];
 		this.state = {
-            suggestions: [],
-            text: '',
-            show:false,
-            name: 'Your Name',
+			suggestions: [],
+			text: '',
+			show: false,
+			name: 'Your Name',
 		};
-    }
-    
-    showModal = () => {
-        this.setState({show:true})
-    }
+	}
 
-    hideModal = () => {
-        this.setState({show:false})
-    }
+	showModal = () => {
+		this.setState({ show: true });
+	};
+
+	hideModal = () => {
+		this.setState({ show: false });
+	};
 
 	onTextChange = (event) => {
 		const value = event.target.value;
 		let suggestions = [];
 		if (value.length > 0) {
 			const regex = new RegExp(`^${value}`, 'i');
-			suggestions = this.schools.sort().filter(v => regex.test(v));
+			suggestions = this.schools.sort().filter((v) => regex.test(v));
 		}
 		this.setState(() => ({ suggestions, text: value }));
 	};
 
+	
 	renderSuggestions() {
 		const { suggestions } = this.state;
 		if (suggestions.length === 0) {
@@ -39,40 +38,35 @@ export default class AutoCompleteText extends React.Component {
 		}
 		return (
 			<ul>
-				{suggestions.map((school) => 
+				{suggestions.map((school) => (
 					<li onClick={() => this.suggestionSelection(school)}>{school}</li>
-				)}
+				))}
 			</ul>
 		);
 	}
 
-    
 	suggestionSelection(value) {
-		this.setState(()=> ({
+		this.setState(() => ({
 			text: value,
 			suggestions: [],
-        }));
-       
-    }
-    
-    captureName = (event) => {
-        const newName = event.target.value
-        this.setState({
-            name: newName
-        })
+		}));
+	}
 
-    }
 	render() {
-        const { text } = this.state;
-        const {name} = this.state
+		const { text } = this.state;
 		return (
 			<div id="welcome">
-                <h3>Hi there! Welcome to your education showcase.</h3>
-                <h3>Type your name and click "Enter" below to begin!</h3>
-                <input value={name} onChange={this.captureName} /><br></br>
-                <button id="enter button">Enter</button><br></br>
+				
+				<h3>Type your name and click "Enter" below to begin!</h3>
+				<input value={this.state.name} onChange={this.captureName} />
+				<br></br>
+				<button id="enter button">Enter</button>
+				<br></br>
+               
+
 				<input value={text} onChange={this.onTextChange} />
                 {this.renderSuggestions()}
+            
 			</div>
 		);
 	}
